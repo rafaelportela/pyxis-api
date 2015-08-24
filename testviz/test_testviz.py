f895caf5642cf8c3f8f454abe6940a4def0e1531
Rafael Portela <rafaelportela90@gmail.com>
2015-08-24 17:26:51 -0300

    Test GET /runs returns 200.

--- testviz/test_testviz.py
+++ testviz/test_testviz.py
@@ -1,16 +1,17 @@
 import unittest
+import pdb
 import testviz
 
 test_database_url = 'mysql://dashboard:password@192.168.33.42/sandbox'
 
-class TestvizTestCase(unittest.TestCase):
+class RunsApi(unittest.TestCase):
   def setUp(self):
     self.app = testviz.app.test_client()
     testviz.init_db(test_database_url)
 
-  def test_index(self):
-    index = self.app.get('/')
-    assert "build-item" in index.data
+  def test_get_runs(self):
+    response = self.app.get('/runs')
+    self.assertEquals(response.status_code, 200)
 
 if __name__ == '__main__':
   unittest.main()
