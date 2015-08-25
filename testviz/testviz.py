207b4033b7c350aeeeebc5f0718c952981419c0e
Juraci <juraci.vieira@gmail.com>
2015-08-25 12:13:33 -0300

    [@Juraci/@Rafaelportela] redirecting root to /runs

--- testviz/testviz.py
+++ testviz/testviz.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect
 from flask import render_template
 from flask import json
 from flask import Response
@@ -7,24 +7,23 @@ import os
 
 app = Flask(__name__)
 
-database_url = os.getenv('DATABASE_URL', 'mysql://dashboard:password@192.168.33.42/sandbox_test')
 
 from database import init_db
 from models import Run
 
 @app.route('/')
 def index():
-  runs = Run.query.limit(5).all()
-  return render_template('index.html', runs = runs)
+  return redirect('/runs', code=302)
 
 @app.route('/runs')
 def runs():
-  runs = Run.query.limit(5).all()
+  runs = Run.query.all()
   data = [run.serialize() for run in runs]
 
   return Response(json.dumps(data), status=200, mimetype="application/json")
 
 if __name__ == '__main__':
+  database_url = os.getenv('DATABASE_URL', 'mysql://dashboard:password@192.168.33.42/sandbox_test')
   init_db(database_url)
   from database import db_session
   app.run(debug=True, host=os.getenv('HOST', 'localhost'))
