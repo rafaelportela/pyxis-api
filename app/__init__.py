f8dc4e34143caaaa241b849a36ebeea7781564d9
Rafael Portela <rafaelportela90@gmail.com>
2015-08-27 13:35:05 -0300

    Allow CORS headers.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask, redirect, json, jsonify, Response, abort
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.cors import CORS
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
+
+CORS(app)
 
 db = SQLAlchemy(app)
 
